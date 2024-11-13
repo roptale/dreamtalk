ea51b57aefb0e7edc45b2ce4a65d6cbe0b130364
Varsik Orchanian <varsenik.ava@gmail.com>
2024-11-13 17:21:15 +0100

    Redirections to landing page + bugfixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-  skip_before_action :check_onboarded, only: [ :home ]
-
-  def home
-  end
-
   def landing
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'messages/create'
-  get 'chatrooms/index'
-  get 'chatrooms/show'
   devise_for :users
-  root to: "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,20 +7,23 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :users do
-    post 'toggle_favorite', on: :member
-    post 'find_or_create_chatroom', on: :member
-  end
+  root to: "users#index"
 
   get "landing", to: "pages#landing"
   get "profile", to: "profile#edit"
   post "update_profile", to: "profile#update"
 
+  get 'messages/create'
+  get 'chatrooms/index'
+  get 'chatrooms/show'
+
+  get '/favorites_profiles', to: 'profile#index', as: 'favorites_profiles'
+  post '/token', to: 'chatrooms#token'
+  resources :users do
+    post 'toggle_favorite', on: :member
+    post 'find_or_create_chatroom', on: :member
+  end
   resources :chatrooms, only: [:index, :show, :new, :create] do
     resources :messages, only: :create
   end
-
-  post '/token', to: 'chatrooms#token'
-  get 'favorites_profiles', to: 'profile#index', as: 'favorites_profiles'
 end
